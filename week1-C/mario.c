b1e889e347b4b75d0e71f74d5486b1c12021400c
deeyalokhande07 <deeyalokhande07@gmail.com>
2025-10-09 14:34:45 +0530

    Add files via upload
    move mario.c to week1-C

---
+++ week1-C/mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+   int height;
+   do
+   {
+    height = get_int("height: ");
+   }
+   while (height < 1 || height > 8);
+   for(int i = 1; i <= height; i++)
+   {
+    for(int j = 0; j < height - i; j++)
+    {
+        printf(" ");
+    }
+    for(int k = 0; k < i; k++)
+    {
+        printf("#");
+    }
+    printf("\n");
+   }
+}
